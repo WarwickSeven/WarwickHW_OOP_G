fd0f448dc2693ebaf8b5f92e0d2c93afca129a4a
WarwickSeven <WarwickSeven@gmail.com>
2022-04-21 17:38:41 +0300

    Initial Commit

---
+++ WarwickHW_OOP_G/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WarwickHW_OOP_G
+//
+//  Created by Александр Тихонов on 21.04.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
